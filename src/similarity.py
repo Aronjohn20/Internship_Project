61bf278511dec4d0c15369c521c45a710f2426b9
Aronjohn20 <aronjohn12112004@gmail.com>
2026-05-19 12:29:52 +0530

    Changes insrc

--- src/similarity.py
+++ src/similarity.py
@@ -8,6 +8,11 @@ from sklearn.metrics.pairwise import (
     cosine_similarity
 )
 
+
+# =========================================================
+# HANDLE MISSING PROCESSED REVIEWS
+# =========================================================
+
 def clean_similarity_data(df):
 
     df['processed_review'] = (
@@ -16,19 +21,36 @@ def clean_similarity_data(df):
     )
 
     return df
-def create_similarity_vectors(processed_reviews):
+
+
+# =========================================================
+# CREATE TF-IDF VECTORS
+# =========================================================
+
+def create_similarity_vectors(
+    processed_reviews
+):
+
     tfidf = TfidfVectorizer(
         max_features=3000,
-        ngram_range=(1,2),
+        ngram_range=(1, 2),
         min_df=2
     )
 
     X_similarity = tfidf.fit_transform(
-        sample_df['processed_review']
+        processed_reviews
     )
+
     return X_similarity
 
-def compute_similarity_matrix(X_similarity):
+
+# =========================================================
+# COMPUTE COSINE SIMILARITY MATRIX
+# =========================================================
+
+def compute_similarity_matrix(
+    X_similarity
+):
 
     similarity_matrix = cosine_similarity(
         X_similarity
@@ -37,12 +59,15 @@ def compute_similarity_matrix(X_similarity):
     return similarity_matrix
 
 
+# =========================================================
+# FIND HIGHLY SIMILAR REVIEW PAIRS
+# =========================================================
+
 def find_similar_reviews(
     sample_df,
     similarity_matrix,
     threshold=0.65
 ):
-    threshold = 0.65
 
     similar_pairs = []
 
@@ -68,21 +93,29 @@ def find_similar_reviews(
                     'reviewer_1': sample_df.iloc[i]['reviewerID'],
                     'reviewer_2': sample_df.iloc[j]['reviewerID'],
 
-
                     'product_1': sample_df.iloc[i]['asin'],
                     'product_2': sample_df.iloc[j]['asin']
                 })
 
-        similarity_results = pd.DataFrame(
+    similarity_results = pd.DataFrame(
         similar_pairs
     )
 
     return similarity_results
 
-def same_product_analysis(similarity_results):
+
+# =========================================================
+# SAME PRODUCT ANALYSIS
+# =========================================================
+
+def same_product_analysis(
+    similarity_results
+):
+
     same_product = similarity_results[
         similarity_results['product_1']
         ==
         similarity_results['product_2']
     ]
+
     return same_product
